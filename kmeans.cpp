5a613277e6f73014f7960315e07ad9a87f32b7fc
dennis <dennis@supergrobi.home>
2023-05-14 08:27:18 +0200

    Added data type, clean structure

--- kmeans.cpp
+++ kmeans.cpp
@@ -1,25 +1,50 @@
 #include <vector>
+#include <cstdlib>
+#include <ctime>
 #include <cmath>
 #include <boost/tuple/tuple.hpp>
 #include "/home/dennis/repos/gnuplot-iostream/gnuplot-iostream.h"
-int main() {
-    Gnuplot gp;
-    std::vector<double> x_coords;
-    std::vector<double> y_coords;
-    std::vector<double> A_x;
-    std::vector<double> A_y;
-    std::vector<double> B_x;
-    std::vector<double> B_y;
+#include <tuple>
 
-    for(double alpha=0; alpha<1; alpha+=1.0/24.0) {
+typedef std::vector<double> VecD;
+
+void initSets(VecD& A_x, VecD& A_y) {
+    
+
+    for(double alpha=0; alpha<1; alpha+=1.0/48.0) {
+        double number = (rand() % 10);
         double theta = alpha*2.0*3.14159;
 
-        B_x.push_back( cos(theta)*0.8);
-        B_y.push_back( sin(theta)*0.8);
-        A_x.push_back( cos(theta)*0.4);
-        A_y.push_back( sin(theta)*0.4);
+        A_x.push_back( cos(theta)*0.4 + number);
+        A_y.push_back( sin(theta)*0.4 + number);
     }
-    gp << "set xrange [-2:2]\nset yrange [-2:2]\n";
+ 
+}
+
+int main() {
+    Gnuplot gp;
+    srand(time(NULL));
+
+    
+    VecD x_coords;
+    VecD y_coords;
+    VecD A_x;
+    VecD A_y;
+    VecD B_x;
+    VecD B_y;
+    
+    initSets(A_x, A_y);
+    double centroid_A_x = A_x[0];
+    double centroid_A_y = A_y[0];
+    double centroid_B_x = B_x[0];
+    double centroid_B_y = B_y[0];
+    double tol = 0.001;
+
+    while(tol>0.001) {
+        
+    }
+
+    gp << "set xrange [-2:10]\nset yrange [-2:10]\n";
     gp << "plot '-' with points title 'B',  '-' with points pt 7 lc rgb 'red' title 'A'\n";
     gp.send1d(boost::make_tuple(B_x, B_y));
     gp.send1d(boost::make_tuple(A_x, A_y));
