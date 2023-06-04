8c0f68138d6069705f6851bfbfc571109e5652ed
dennis <dennis.cronbach@posteo.de>
2023-06-04 21:53:49 +0200

    fixed a bug and completed loop

--- kmeans.cpp
+++ kmeans.cpp
@@ -1,3 +1,5 @@
+//g++ kmeans.cpp -std=c++17 -lboost_iostreams -lboost_system -lboost_filesystem
+
 #include <vector>
 #include <cstdlib>
 #include <ctime>
@@ -53,25 +55,47 @@ int main() {
     VecD B_x_new;
     VecD A_y_new;
     VecD B_y_new;
+    double centroid_B_x;
+    double centroid_B_y; 
     
     initSets(A_x, A_y);
     double centroid_A_x = A_x[0];
     double centroid_A_y = A_y[0];
-    double centroid_B_x = B_x[0];
-    double centroid_B_y = B_y[0];
+    if(B_x.size() != 0) {
+        centroid_B_x = B_x[0];
+        centroid_B_y = B_y[0];
+    } else {
+        centroid_B_x = 0.0;
+        centroid_B_y = 0.0;
+    }
+    
     double tol = 0.001;
+    int count = 0;
 
     //start of algorithm
-    while(tol>0.001) {
+    while(count < 4) {
         float distanceA;
         double distanceB;
         for(int i=1; i<A_x.size();i++) {
-            distanceA = sqrt(pow(A_x[i]-A_x[0], 2)+(pow(A_y[i]-A_y[0], 2)));
-            distanceB = sqrt(pow(B_x[i]-B_x[0], 2)+(pow(B_y[i]-B_y[0], 2)));
+            distanceA = sqrt(pow(A_x[i]-centroid_A_x, 2)+(pow(A_y[i]-centroid_A_y, 2)));
+            distanceB = sqrt(pow(A_x[i]-centroid_B_x, 2)+(pow(A_y[i]-centroid_B_y, 2)));
 
             if(distanceA<=distanceB) {
                 A_x_new.push_back(A_x[i]);
                 A_y_new.push_back(A_y[i]);
+            }
+            else {
+                B_x_new.push_back(A_x[i]);
+                B_y_new.push_back(A_y[i]); 
+            }
+        }
+        for(int i=1; i<B_x.size();i++) {
+            distanceA = sqrt(pow(B_x[i]-centroid_A_x, 2)+(pow(B_y[i]-centroid_A_y, 2)));
+            distanceB = sqrt(pow(B_x[i]-centroid_B_x, 2)+(pow(B_y[i]-centroid_B_y, 2)));
+
+            if(distanceA<=distanceB) {
+                A_x_new.push_back(B_x[i]);
+                A_y_new.push_back(B_y[i]);
             }
             else {
                 B_x_new.push_back(B_x[i]);
@@ -79,14 +103,41 @@ int main() {
             }
         }
 
-        calculateCentroids(A_x_new, A_y_new);
-        calculateCentroids(B_x_new, B_y_new);
+        vector<float> centroidA = calculateCentroids(A_x_new, A_y_new);
+        centroid_A_x = centroidA[0];
+        centroid_A_y = centroidA[1];
+        vector<float> centroidB = calculateCentroids(B_x_new, B_y_new);
+        centroid_B_x = centroidB[0];
+        centroid_B_y = centroidB[1];
+
+        A_x = A_x_new;
+        A_y = A_y_new;
+        B_x = B_y_new;
+        B_y = B_y_new;
+        count++;
+        std::cout << centroid_A_x;
+        std::cout << "---";
+        std::cout << centroid_A_y;
+        std::cout << "---";
+       
+        
+    }
+        
 
 
+         VecD printcentroidx;
+        VecD printcentroidy;
+        printcentroidx.push_back(centroid_A_x);
+        printcentroidx.push_back(centroid_B_x);
+        printcentroidx.push_back(centroid_A_y);
+        printcentroidx.push_back(centroid_B_y);
+        gp << "set xrange [-2:10]\nset yrange [-2:10]\n";
+        gp << "plot '-' with points title 'B',  '-' with points pt 7 lc rgb 'red' title 'A'\n";
+        gp.send1d(boost::make_tuple(B_x, B_y));
+        gp.send1d(boost::make_tuple(A_x, A_y));
+        //gp.send1d(boost::make_tuple(printcentroidx, printcentroidy));
+        ///gp.send1d(boost::make_tuple(centroid_B_x, centroid_B_y));
+        
 
-
-    gp << "set xrange [-2:10]\nset yrange [-2:10]\n";
-    gp << "plot '-' with points title 'B',  '-' with points pt 7 lc rgb 'red' title 'A'\n";
-    gp.send1d(boost::make_tuple(B_x, B_y));
-    gp.send1d(boost::make_tuple(A_x, A_y));
+    
 } // very simple tool right???
